86a39717ccb2d0e698340e07fbe682391ed13404
Chubbard022 <chubbard022@gmail.com>
2019-05-10 09:09:24 -0700

    Login component at barebones

---
+++ dad-jokes/src/auth/Login.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+class Login extends React.Component{
+    state={
+        username: "url5",
+        password: "pass"
+
+    }
+    render(){
+        return(
+            <div>
+                Hello from Login
+            </div>
+        )
+    }
+}
+
+
+
+export default Login;
