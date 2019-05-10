3bdaa938cd4647b939d18cfef4c59d40d98fd7ce
Chubbard022 <chubbard022@gmail.com>
2019-05-10 08:39:45 -0700

    routes finished plus generateToken function

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const { authenticate } = require('../auth/authenticate');
+const secret = require("../auth/secret")
+const Users = require("../auth/authenticate")
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,12 +13,49 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(user=>{
+      res.status(201).json({successMessage: "SUCCESS in registering"})
+    })
+    .catch(err=>{
+      res.status(500).json({errorMessage:"ERROR occurred when registering"})
+    })  
 }
 
 function login(req, res) {
-  // implement user login
+  let {username, password} = req.body;
+
+  Users.findBy({username})
+    .first()
+    .then(user=>{
+      if(user && bcrypt.compareSync(password,user.password)){
+        const token = generateToken(user);
+
+        res.status(200).json({welcomeMessage: "welcome, enjoy the dad jokes",token})
+      }else{
+        res.status(401).json({errorMessage:"error, username and password don't work"})
+      }
+    })
+    .catch(err=>{
+      res.status(500).json({errorMessage:"unable to register account "})
+    })
 }
+
+function generateToken(user){
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+  const options = {
+    expiresIn: "2h"
+  }
+  return jwt.sign(payload,secret,options)
+}
+
 
 function getJokes(req, res) {
   const requestOptions = {
